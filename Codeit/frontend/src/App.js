f9246f131c8e6d9e810dc7d485dbc298ee9dd2d2
ashwaniponia <ashwaniponia@gmail.com>
2020-12-21 14:17:16 +0530

    Front End Basic Structure created

---
+++ Codeit/frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SignIn from './Signin';
+
+function App() {
+  return (
+    <div className="Header">
+      {/* Header  */}
+      <SignIn />
+      {/* App Body */}
+      {/* SideBar */}
+      {/* Feed  */}
+    </div>
+  );
+}
+
+export default App;
