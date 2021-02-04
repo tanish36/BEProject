c352d64df05dc986aa5afa697b2dfae202b219fd
tanish36 <tanish.dashore123@gmail.com>
2021-02-04 13:27:12 +0530

    modified

--- contest/models.py
+++ contest/models.py
@@ -6,7 +6,7 @@ class contest2(models.Model):
     contestid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     duration = models.IntegerField()
     title = models.CharField(max_length=200)
-    timestamp = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField()
 
 class contestproblem(models.Model):
     contestid = models.CharField(max_length=200)
