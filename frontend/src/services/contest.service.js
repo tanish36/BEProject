edc997c6fe4399c1d81007894002a4e567e406da
ashwaniponia <ashwaniponia@gmail.com>
2021-02-08 15:37:46 +0530

    Contest Latest

--- frontend/src/services/contest.service.js
+++ frontend/src/services/contest.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "http://127.0.0.1:8000/contest/";
 
 class ContestService {
-// "getallcontest"
+    // "getallcontest"
     getcproblem(cp_id) {
         return axios
             .get(API_URL + "getcproblem", {
@@ -18,61 +18,38 @@ class ContestService {
             });
     }
 
-    //get all contest
-    getcontest() {
-        return axios
-            .get(API_URL + "getallcontest")
-            .then(response => {
-                console.log(response);
-                localStorage.setItem("Contests", JSON.stringify(response.data));
-                return response.data;
-            });
-        }
-
-
-    //get all problem 
-
-    getproblems() {
-        return axios
-            .get("http://127.0.0.1:8000/problems/getproblems")
-            .then(response => {
-                //console.log(response);
-                localStorage.setItem("Problems", JSON.stringify(response.data));
-                return response.data;
-            });
-        }
-
-
     // TO-DO
     registeruser() {
 
     }
 
-    addcontest(duration,title,timestamp) {
+    addcontest(duration, title, timestamp) {
         return axios
-        .post(API_URL + "addcontest", {
-            duration,title,timestamp
-        })
-        .then(response => {
-            console.log(response);
-            return response.data;
-        });
+            .post(API_URL + "addcontest", {
+                duration, title, timestamp
+            })
+            .then(response => {
+                console.log(response);
+                return response.data;
+            });
 
     }
 
-    addproblem(problem_name,problem_statement,problem_tags ,problem_io, problem_con,problem_test) {
+    addproblem(problem_name, problem_statement, problem_tags, problem_io, problem_con, problem_test) {
         return axios
-        .post("http://127.0.0.1:8000/problems/addproblem", {
-           problem_name,problem_statement,problem_tags ,problem_io, problem_con,problem_test
-        })
-        .then(response => {
-            console.log(response);
-            return response.data;
-        });
+            .post("http://127.0.0.1:8000/problems/addproblem", {
+                problem_name, problem_statement, problem_tags, problem_io, problem_con, problem_test
+            })
+            .then(response => {
+                console.log(response);
+                return response.data;
+            });
 
     }
 
-    
+    addprobleminContest() {
+
+    }
 
 }
 
