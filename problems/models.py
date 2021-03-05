5dc10b1037a2581deda93c63ac22d874a9355342
tanish36 <tanish.dashore123@gmail.com>
2021-03-05 15:34:09 +0530

    problem fix

--- problems/models.py
+++ problems/models.py
@@ -4,8 +4,17 @@ class problem(models.Model):
     problem_id = models.AutoField(primary_key=True)
     problem_name = models.TextField()
     problem_statement = models.TextField()
-    problem_tags = models.TextField()
-    problem_io = models.TextField()#example
-    problem_con = models.TextField()#constrains
-    problem_test = models.TextField()#test case 
+    problem_tags = models.TextField(null=True)
+    problem_input = models.TextField(null=True)#example
+    problem_output = models.TextField(null=True)#constrains
+    problem_example = models.TextField(null=True)#test case
+    problem_samplecase = models.TextField(null=True)
+    problem_score = models.TextField(null=True) 
+    problem_noofsubmission = models.TextField(null=True)
+
+class problemfeedback(models.Model):
+    problem_feedback = models.TextField()
+    email = models.TextField()
+    problem_id = models.IntegerField()
+
 
--- problems/urls.py
+++ problems/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 
     path('getproblems',views.getproblems,name = 'getproblems'),
     path('getproblemsid',views.getproblemsid,name = 'getproblemsid'),
-    path('addproblem',views.addproblem,name = 'addproblem')
+    path('addproblem',views.addproblem,name = 'addproblem'),
+    path('problemfeedback',views.problemfeedback,name = 'problemfeedback')
 ]
