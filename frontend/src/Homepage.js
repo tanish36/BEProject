5cde43c35ed6727c76d30690b50226741016e7d9
ashwaniponia <ashwaniponia@gmail.com>
2021-03-13 09:33:13 +0530

    Welcome Path Changed

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Card, Row, Col, Form, Button, Container } from 'react-bootstrap';
-import DoughnutGraph from './Dougnut'
-import LineGraph from './Line'
-import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import Home from './Home.js'
 
 function Homepage() {
 
     return (
         <div>
-            <Home/>
+            <Home />
         </div>
     )
 }
